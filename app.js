3f0c71cf6c18d84ffd222ec6009b1f6a56aedbe9
Javier <javier96rn@gmail.com>
2022-01-23 16:21:57 +0100

    Finish resizing button logic

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 const container = document.querySelector('.container');
 const clearBtn = document.querySelector('.clear-btn');
-const gridRange = document.querySelector('.grid-range');
+const gridBtn = document.querySelector('.grid-btn');
 
 function createGrid(nBoxes) {
-    container.style.gridTemplateRows = `repeat(${Math.sqrt(nBoxes)}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${Math.sqrt(nBoxes)}, 1fr)`;
-    for (let i = 0; i <= nBoxes - 1; i++) {
-        const newBox = document.createElement('div');
-        newBox.classList.add('box');
-        container.append(newBox);
+    if (Math.sqrt(nBoxes) > 4 && Math.sqrt(nBoxes) < 50) {
+        const boxes = document.querySelectorAll('.box');
+        boxes.forEach(box => {
+            box.remove();
+        })
+        container.style.gridTemplateRows = '';
+        container.style.gridTemplateColumns = '';
+        container.style.gridTemplateRows = `repeat(${Math.sqrt(nBoxes)}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${Math.sqrt(nBoxes)}, 1fr)`;
+        for (let i = 0; i <= nBoxes - 1; i++) {
+            const newBox = document.createElement('div');
+            newBox.classList.add('box');
+            container.append(newBox);
+        }
+        addEvent();
+    } else {
+        alert('Please select a size between 4 and 50');
     }
+
+}
+
+function addEvent() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', function () {
+            box.classList.add('hovered');
+        })
+    })
 }
 
 createGrid(256);
 
 const boxes = document.querySelectorAll('.box');
 
-boxes.forEach(box => {
-    box.addEventListener('mouseover', function (e) {
-        box.classList.add('hovered');
-    })
-})
+
 
 clearBtn.addEventListener('click', function () {
     boxes.forEach(box => {
@@ -28,10 +45,8 @@ clearBtn.addEventListener('click', function () {
     })
 })
 
-gridRange.addEventListener('input', function (e) {
-    boxes.forEach(box => {
-        box.remove();
-    })
-    createGrid(e.target.value);
+gridBtn.addEventListener('click', function () {
+    const size = prompt('Please select a new size (4 - 50): ')
+    createGrid(size * size);
 })
 
